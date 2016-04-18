89ca31df07575e537976b1a0bf26f4dce846a96c
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-04-18 12:48:18 -0400

    start of app

---
+++ TableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TableViewController.swift
+//  harvest
+//
+//  Created by Philippe Kimura-Thollander on 4/18/16.
+//  Copyright © 2016 Philippe Kimura-Thollander. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.tableFooterView = UIView()
+        tableView.scrollEnabled = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+
+
+}
